873f319fbedc27d062bd032c3c66a689ec589305
unknown <subashchandraboseravi45@gmail.com>
2025-08-11 20:22:05 +0530

    update

--- backend/src/controllers/adminController.ts
+++ backend/src/controllers/adminController.ts
@@ -1744,6 +1744,7 @@ const getAllPaymentIntent = async (req: express.Request, res: express.Response) 
 
   try {
     const paymentIntent = await prisma.paymentIntent.findMany({
+      where: { isSoftDeleted: false },
       include: {
         bookings: {
           select: {
