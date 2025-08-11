bd43edebe9049276ae9232fe803ee378836d35dc
unknown <subashchandraboseravi45@gmail.com>
2025-08-11 20:42:52 +0530

    prodt today updates final

--- backend/src/controllers/automatedTaskRuleController.ts
+++ backend/src/controllers/automatedTaskRuleController.ts
@@ -11,7 +11,6 @@ export const getAutomatedTaskRules = async (req: Request, res: Response) => {
   }
 };
 
-
 export const createAutomatedTaskRule = async (req: Request, res: Response) => {
   try {
     const rule = await notificationService.createAutomatedTaskRule(req.body);
